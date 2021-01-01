3fa08d51e75528ed31eea367b02fc4d738a5ec73
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-01 13:46:37 -0800

    Add functionality for pausing song

--- script.js
+++ script.js
@@ -34,6 +34,15 @@ function playSong() {
   audio.play();
 }
 
+// Pause song
+function pauseSong() {
+  musicContainer.classList.add('pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
