79773a5f8503f6ad482fd60272cd9f677530f21a
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-01 16:02:39 -0800

    Handle song end

--- script.js
+++ script.js
@@ -118,3 +118,6 @@ audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
